a5d682cafdbf0d8b0abee408071a0b8b2db9ddc8
mmddxxx <1066437968@qq.com>
2022-07-15 16:08:55 +0800

    将barrier-free_feddback_system项目添加到本地库里

---
+++ src/main/java/com/example/barrierfree_feddback_system/BarrierFreeFeddbackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.barrierfree_feddback_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierFreeFeddbackSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarrierFreeFeddbackSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barrierfree_feddback_system/BarrierFreeFeddbackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barrierfree_feddback_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierFreeFeddbackSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
